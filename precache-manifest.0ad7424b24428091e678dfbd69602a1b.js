13035614dec803b77281fc47020b95d13ab5903b
premsahni0501 <premsahni0501@gmail.com>
2020-07-29 18:12:49 +0530

    Updates

---
+++ precache-manifest.0ad7424b24428091e678dfbd69602a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88fad6fe658540db76b57b06346244c",
+    "url": "/chatboat-editor/index.html"
+  },
+  {
+    "revision": "de787b88f8f6e64be2d3",
+    "url": "/chatboat-editor/static/css/main.9a3f8a70.chunk.css"
+  },
+  {
+    "revision": "93a567c6371e96ad9ce5",
+    "url": "/chatboat-editor/static/js/2.e84837cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chatboat-editor/static/js/2.e84837cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de787b88f8f6e64be2d3",
+    "url": "/chatboat-editor/static/js/main.a7c92233.chunk.js"
+  },
+  {
+    "revision": "5bda9d7eb5c8f6014c28",
+    "url": "/chatboat-editor/static/js/runtime-main.90d8ab85.js"
+  }
+]);
